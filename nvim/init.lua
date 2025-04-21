3520fb778363ca72d94c0f7ba2d65e41305d009f
Sami Salmi <samipetersalmi@gmail.com>
2025-04-22 02:09:36 +0300

    fine tuning

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,11 @@ require("config.lazy")
 require("options")
 require("alpha-config")
 
-vim.diagnostic.config({ virtual_lines = true })
+vim.diagnostic.config({
+  virtual_lines = {
+    current_line = true,
+  },
+})
 
 vim.lsp.enable({
   "lua_ls",
