d21dcf665ebd8d3aefae601d1c0ce9e91e2643bf
Sami Salmi <samipetersalmi@gmail.com>
2025-05-17 15:48:10 +0300

    commit

--- doom/config.el
+++ doom/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;;(setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -87,5 +87,15 @@
   (setq xclip-mode t)
   (setq xclip-method (quote wl-copy)))
 
+(define-key global-map (kbd "M-k") nil)
+(define-key evil-insert-state-map (kbd "M-k") nil)
+
+(use-package! drag-stuff
+  :config
+  (drag-stuff-mode 1)
+  (map! :n "M-j" #'drag-stuff-down
+        :n "M-k" #'drag-stuff-up
+        :v "M-j" #'drag-stuff-down
+        :v "M-k" #'drag-stuff-up))
 
 (load-theme 'gruber-darker t)
